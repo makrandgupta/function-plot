df56b0326de2d408dd1e948f0d08486cadb8ed48
Makrand Gupta <makrand1996@gmail.com>
2017-05-26 12:48:38 -0400

    moved axis labels to outside graph

--- site/js/playground.js
+++ site/js/playground.js
@@ -9,56 +9,89 @@
 var cursor = 0;
 var fp = functionPlot(makeSettings(0));
 
-var mouseMoveListener = fp.listeners('mousemove')[0];
-fp.removeListener('mousemove', mouseMoveListener);
-fp.on('updateTip', mouseMoveListener);
-
-var mouseOutListener = fp.listeners('mouseout')[0];
-fp.removeListener('mouseout', mouseOutListener);
+// var mouseMoveListener = fp.listeners('mousemove')[0];
+// fp.removeListener('mousemove', mouseMoveListener);
+// fp.on('updateTip', mouseMoveListener);
 
 // var mouseOutListener = fp.listeners('mouseout')[0];
 // fp.removeListener('mouseout', mouseOutListener);
 
-document.addEventListener('keydown', function(event) {
-	// console.log(event)
-	switch (event.key) {
-		case 'ArrowRight':
-			cursor++;
-			console.log('right trigger', cursor)
-			break;
-		case 'ArrowLeft':
-			cursor--;
-			console.log('left trigger', cursor);
-			break;
-		default:
-			return;
-	}
-	console.log(makeSettings(cursor))
-	functionPlot(makeSettings(cursor));
-	fp.emit('updateTip', {x: cursor, y: 0});
-}, false);
+// // var mouseOutListener = fp.listeners('mouseout')[0];
+// // fp.removeListener('mouseout', mouseOutListener);
+
+// document.addEventListener('keydown', function(event) {
+// 	// console.log(event)
+// 	switch (event.key) {
+// 		case 'ArrowRight':
+// 			cursor++;
+// 			console.log('right trigger', cursor)
+// 			break;
+// 		case 'ArrowLeft':
+// 			cursor--;
+// 			console.log('left trigger', cursor);
+// 			break;
+// 		default:
+// 			return;
+// 	}
+// 	console.log(makeSettings(cursor))
+// 	functionPlot(makeSettings(cursor));
+// 	fp.emit('updateTip', {x: cursor, y: 0});
+// }, false);
 
 function makeSettings(cursor) {
 	return {
   target: '#playground',
   width: 300,
   height: 180,
-  xTickValues: [1, 2, 3, 4],
-  yTickValues: [-1, -2, -3, 4],
+  // xTickValues: [1, 2, 3, 4],
+  // yTickValues: [-1, -2, -3, 4],
   // ticks: 4,
 
   grid: true,
   tip: {
   	xLine: true,
   	yLine: true,
+    renderer: function(x, y, index) {
+      return x + ", " + y;
+    }, 
   },
+  xAxis: {
+  	domain: [0, 1200],
+    label: 'vol'
+  },
+	yAxis: {
+  	domain: [0, 40],
+    label: 'height'
+  },
+  xTicks: 6,
   data: [
     { 
-    	fn: '2^x', 
+    	fn: 'x/40', 
     	sampler: 'builtIn', 
     	graphType: 'polyline',
-    	range: [cursor, cursor+0.0001]
-    }
+    	range: [0, 800],
+    	color: 'red',
+    },
+    {
+    	fn: '((x-200)/200)^2 + 11', 
+    	sampler: 'builtIn', 
+    	graphType: 'polyline',
+    	range: [800, 1200]
+    },
+    // { 
+    // 	fn: '(x/20) - 23', 
+    // 	sampler: 'builtIn', 
+    // 	graphType: 'polyline',
+    // 	range: [1000, 1200],
+    // 	color: 'red',
+    // },
+    // { 
+    // 	fn: '(x/40)', 
+    // 	sampler: 'builtIn', 
+    // 	graphType: 'polyline',
+    // 	range: [0, 1200],
+    // 	color: 'blue',
+    // }
   ]
 };
 }
